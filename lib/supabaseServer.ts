ffde032460492e4803b4ddd721b82143f4639b7b
Nico <160150414+NinikCui@users.noreply.github.com>
2025-11-15 11:11:16 +0700

    Revert "Fix"
    This reverts commit 0675c24ea70f1b7e4dc184fa11fac51cdc0d0698.

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -6,9 +6,8 @@ export async function supabaseServer() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
